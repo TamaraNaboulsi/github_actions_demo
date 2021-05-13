557f5a63b86187e8086fbebd2a2edb2731ad5ca5
Tamara El Naboulsi <48948777+TamaraNaboulsi@users.noreply.github.com>
2021-05-13 18:44:33 +0100

    Create run_tests.pl

---
+++ run_tests.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Test::More tests => 5;
+
+print "Running tests\n";
+
+
+# # or
+# use Test::More skip_all => $reason;
+# # or
+# use Test::More;   # see done_testing()
+ 
+require_ok('Devel::Trace');
+
+ok(1 + 2 == 3, '1 + 2 == 3');
+ok(2 * 4 == 8, '2 * 4 == 8');
+
+my $divide = 5 / 3;
+ok(abs($divide - 1.666667) < 0.001, '5 / 3 == (approx) 1.666667');
+
+my $subtract = -3 + 3;
+ok(($subtract eq "0" or $subtract eq "-0"), '-3 + 3 == 0');
